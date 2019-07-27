d68cb924ebea2d21353622697e4cc9f6a1b667cd
GreenNeptune <silentwisdom75@gmail.com>
2019-07-27 11:08:11 +0300

    create a category end point

--- store.py
+++ store.py
@@ -1,24 +1,55 @@
-from bottle import route, run, template, static_file, get, post, delete, request
+from bottle import route, run, template, static_file, get, post, delete, request,response
 from sys import argv
 import json
 import pymysql
 
+# pymysql connector 
+connection = pymysql.connect(host="localhost",    
+                     user="root",        
+                     passwd="root", 
+                     db="store",
+                     charset="utf8",
+                     cursorclass=pymysql.cursors.DictCursor)
+
+
 # categories endpoints
 
-
 @post("/category")
-def create_category(category_name):
-    pass
+def create_category():
+  with connection.cursor() as cursor:
+    try:
+      category_name = request.json.get("name")
+      
+      if category_name is None:
+        response.status = 400
+        response["status__line"] = "name parameter is missing"
+        return response
+      
+      search_category_query = f'select cat_name from categories where cat_name = "{category_name}"'
+      cursor.execute(search_category_query)
+      category_exists = cursor.fetchone() 
+      if category_exists:
+        response.status = 200
+        response["status__line"] = "category already exists"
+        return response
 
-
-@delete("/category/<id>")
+      sql = 'INSERT INTO categories VALUES(null,"{}")'.format(category_name)
+      connection.commit()
+      response.status = 201
+      response["status__line"] = "category created successfully"
+      return response
+    except Exception as e:
+      response.status = 500
+      response["status__line"] = "internal Error"
+      return response
+            
+@delete("/category/<id:int>")
 def delete_category(id):
-    pass
-
+  pass     
 
 @get("/categories")
 def get_all_categories():
-    pass
+  pass
 
 # products endpoints
 
